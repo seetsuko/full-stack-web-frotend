98f53626c242103f27f41a36d4b3771e3d34280e
seetsuko <radgjms@gmail.com>
2024-03-04 15:00:31 +0000

    feat:フロントエンド環境構築

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports ={
+  async rewrites(){
+    return[
+      {
+        source: "/api/:path*",
+        destination: "http://host.docker.internal:8000/api/:path*/",
+      }
+    ]
+  }
+}
