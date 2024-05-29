614b06a05620865e16327321d52794d5f685165c
seetsuko <radgjms@gmail.com>
2024-05-29 15:26:47 +0000

    fix:商品登録部分のコーディング

--- app/inventory/products/page.tsx
+++ app/inventory/products/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
+import Link from "next/link"
 import productsData from "./sample/dummy_products.json"
 
 type ProductsData = {
@@ -30,16 +31,29 @@ useEffect(()=>{
             <th>単価</th>
             <th>説明</th>
             <th></th>
+            <th></th>
           </tr>
         </thead>
 
         <tbody>
-          {data.map((data:any)=>(
             <tr>
+              <td></td>
+              <td><input type='text'/></td>
+              <td><input type='number'/></td>
+              <td><input type='text'/></td>
+              <td></td>
+              <td>
+                <button>キャンセル</button>
+                <button>登録する</button>
+              </td>
+            </tr>
+          {data.map((data:any)=>(
+            <tr key={data.id}>
               <td>{data.id}</td>
               <td>{data.name}</td>
               <td>{data.price}</td>
               <td>{data.description}</td>
+              <td><Link href={`/inventory/products/${data.id}`}>在庫処理</Link></td>
               <td><button>更新・削除</button></td>
             </tr>
           ))}
