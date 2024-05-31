8817532e0d952d74d94f075675dcecdb40c42747
seetsuko <radgjms@gmail.com>
2024-05-31 15:16:26 +0000

    fix:べた打ち箇所を決してmapでinventryDataを展開

--- app/inventory/products/[id]/page.tsx
+++ app/inventory/products/[id]/page.tsx
@@ -48,7 +48,7 @@ export default function Page() {
       <form>
         <div>
           <label>商品名：</label>
-          <span>コットン100%バックリボンティアードワンピース（黒）</span>
+          <span>product.name</span>
         </div>
         <div>
           <label>数量：</label>
@@ -70,46 +70,16 @@ export default function Page() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>卸し</td>
-              <td>2023-04-03 18:54:13</td>
-              <td>6900</td>
-              <td>2</td>
-              <td>13800</td>
-              <td>390</td>
+            {data.map((data: InventoryData) => (
+              <tr key={data.id}>
+              <td>{data.type}</td>
+              <td>{data.date}</td>
+              <td>{data.unit}</td>
+              <td>{data.quantity}</td>
+              <td>{data.price}</td>
+              <td>{data.inventory}</td>
             </tr>
-            <tr>
-              <td>仕入れ</td>
-              <td>2023-04-03 18:54:13</td>
-              <td>6900</td>
-              <td>3</td>
-              <td>20700</td>
-              <td>392</td>
-            </tr>
-            <tr>
-              <td>卸し</td>
-              <td>2023-04-03 18:54:13</td>
-              <td>6900</td>
-              <td>1</td>
-              <td>6900</td>
-              <td>389</td>
-            </tr>
-            <tr>
-              <td>卸し</td>
-              <td>2023-04-03 18:54:13</td>
-              <td>6900</td>
-              <td>10</td>
-              <td>69000</td>
-              <td>390</td>
-            </tr>
-            <tr>
-              <td>仕入れ</td>
-              <td>2023-04-03 18:54:13</td>
-              <td>6900</td>
-              <td>400</td>
-              <td>2760000</td>
-              <td>400</td>
-            </tr>
+            ))}
           </tbody>
         </table>
     </>
